7a72e66cb538c7765c76d5e3652613ae2a195624
m1guel3agle <mian.ag123@gmail.com>
2026-02-08 20:16:20 -0500

    modelo movie y admin

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from movie import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('about/', views.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
